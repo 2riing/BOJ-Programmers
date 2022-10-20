bb6fe0b0f8b81c788ffce30c6dcf53f059c2d20a
2riing <5032303000@naver.com>
2022-10-20 22:18:05 +0900

    [BOJ] 15656 N과 M (7)

---
+++ [BOJ] 15656 N과 M (7).py
@@ -0,0 +1,17 @@
+import sys
+sys.stdin = open('input.txt')
+
+def DFS():
+    if len(arr) == M:
+        print(*arr)
+        return
+    for i in range(N):
+        arr.append(numbers[i])
+        DFS()
+        arr.pop()
+
+N, M = map(int, input().split())
+numbers = list(map(int, input().split()))
+numbers.sort()
+arr = []
+DFS()
