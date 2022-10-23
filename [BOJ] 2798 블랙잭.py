dc4c9349efb77f0ddaa294a31c6601906c5fb22e
2riing <5032303000@naver.com>
2022-10-23 21:10:02 +0900

    [BOJ] 2798 블랙잭

---
+++ [BOJ] 2798 블랙잭.py
@@ -0,0 +1,14 @@
+import sys
+sys.stdin = open('input.txt')
+
+N, M = map(int, input().split())
+cards = list(map(int, input().split()))
+
+max_num = 0
+for i in range(N -2):
+    for j in range(i + 1, N-1 ):
+        for k in range(j + 1, N):
+            if cards[i] + cards[j] + cards[k] > M:
+                continue
+            max_num = max(max_num, cards[i] + cards[j] + cards[k])
+print(max_num)
