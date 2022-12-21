e5223151a90e71e234f053c56b7155fb5ef81b9d
2riing <5032303000@naver.com>
2022-12-21 21:14:07 +0900

    [BOJ] 10825 국영수

---
+++ [BOJ] 10825 국영수.py
@@ -0,0 +1,10 @@
+import sys
+sys.stdin = open('input.txt')
+
+N = int(input())
+students = [ input().split() for _ in range(N)]
+students.sort(key = lambda x:(-int(x[1]), int(x[2]), -int(x[3]), x[0]))
+
+for student in students:
+    print(student[0])
+
