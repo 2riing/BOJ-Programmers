af4a2123cb23fb0ec979dbd03b9d2da271428fa9
2riing <5032303000@naver.com>
2022-12-23 21:34:00 +0900

    [BOJ] 2446 별찍기 - 9

---
+++ [BOJ] 2446 별찍기 - 9.py
@@ -0,0 +1,10 @@
+import sys
+sys.stdin = open('input.txt')
+
+N = int(input())
+
+for i in range(N):
+    print(' '*i, '*'*((N-i)*2-1))
+
+for i in range(N-2, -1, -1):
+    print(' '*i, '*'*((N-i)*2-1))
